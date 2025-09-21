34d42e9a2ed7e2bb9e91e94bfc2c1a1e1e4bf930
Jaehee Kwon <jaehee.kwon@gmail.com>
2025-09-21 14:12:30 -0700

    Add mks_canble_v10_pro

---
+++ boards/makerbase/mks_canable_v10_pro/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2024 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=STM32F072CB" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
